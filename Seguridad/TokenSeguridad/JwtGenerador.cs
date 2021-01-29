31b155ebc22ffb0d2fd4f651ad4164f4b8ec5ec3
Facundo <facundomarcos@live.com.ar>
2021-01-29 00:27:29 -0300

    66 - crear token

--- Seguridad/TokenSeguridad/JwtGenerador.cs
+++ Seguridad/TokenSeguridad/JwtGenerador.cs
@@ -1,5 +1,11 @@
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Aplicacion.Contratos;
 using Dominio;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
 
 namespace Seguridad
 {
@@ -8,7 +14,28 @@ namespace Seguridad
     {
         public string CrearToken(Usuario usuario)
         {
-            throw new System.NotImplementedException();
+            var claims = new List<Claim>{
+                new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName)
+            };
+            //crear credenciales de acceso
+            //palabra secreta que va a desencriptar el token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+            //
+            var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescripcion = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(30),
+                SigningCredentials = credenciales
+                
+            };
+
+            var tokenManejador = new JwtSecurityTokenHandler();
+            //el token se basa en la descripcion declarada arriba para poder ser creado
+            var token = tokenManejador.CreateToken(tokenDescripcion);
+
+            return tokenManejador.WriteToken(token);
+                
         }
     }
 }
