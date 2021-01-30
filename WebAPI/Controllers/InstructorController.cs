824377a9a9cfb9bdeefcf22b8f0520d541b2e92c
Facundo <facundomarcos@live.com.ar>
2021-01-30 11:27:08 -0300

    99 -  crear instructor -procedimiento almacenado

--- WebAPI/Controllers/InstructorController.cs
+++ WebAPI/Controllers/InstructorController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Aplicacion.Instructores;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Persistencia.DapperConexion.Instructor;
 
@@ -8,9 +9,17 @@ namespace WebAPI.Controllers
 {
     public class InstructorController : MiControllerBase
     {
+        // http://localhost:5000/api/Instructor
         [HttpGet]
         public async Task<ActionResult<List<InstructorModel>>> ObtenerInstructores(){
             return await Mediator.Send(new Consulta.Lista());
         }
+
+        // http://localhost:5000/api/Instructor
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data){
+            return await Mediator.Send(data);
+        }
+        
     }
 }
