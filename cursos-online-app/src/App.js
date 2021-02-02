b3ed2a3e8021cda0ab40b0ee741597ff3bf21be5
Facundo <facundomarcos@live.com.ar>
2021-02-02 13:56:31 -0300

    147 - implementacion del theme

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+//entrega las librerias de material design 
+import MuithemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+//importar el tema con los parametros que definimos del theme
+import theme from "./theme/theme";
+import { Button, TextField } from '@material-ui/core';
 
 function App() {
 return (
-  <h1>Proyecto en blanco</h1>
+  <MuithemeProvider theme = {theme}>
+    <h1>Proyecto en blanco</h1>
+    <TextField variant = "outlined"/>
+    <Button variant="contained" color="primary">Mi boton material design</Button>
+  </MuithemeProvider>
+  
 )
 }
 
--- cursos-online-app/src/theme/theme.js
+++ cursos-online-app/src/theme/theme.js
@@ -4,8 +4,11 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
     palette : {
         primary : {
+            //
             light : "#63afff",
+            //azul claro 
             main : "#1976d2",
+            //azul oscuro foco
             dark : "#004ba0",
             getContrastText : "#ecfad8"
         }
