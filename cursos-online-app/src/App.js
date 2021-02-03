0579be963b6157cf19d7ba9f538cd3715176a930
Facundo <facundomarcos@live.com.ar>
2021-02-02 23:41:35 -0300

    153 - crear perfil de usuario

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import {ThemeProvider as MuithemeProvider}  from "@material-ui/core/styles";
 //importar el tema con los parametros que definimos del theme
 import theme from "./theme/theme";
-import RegistrarUsuario from './componentes/seguridad/RegistrarUsuario';
+//import RegistrarUsuario from './componentes/seguridad/RegistrarUsuario';
+//import Login from './componentes/seguridad/Login';
+import PerfilUsuario from './componentes/seguridad/PerfilUsuario';
 
 function App() {
 return (
   <MuithemeProvider theme={theme}>
-    <RegistrarUsuario/>
+    <PerfilUsuario />
   </MuithemeProvider>
   
 )
